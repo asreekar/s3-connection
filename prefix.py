68ef7f64245311dacc985996c70038de0f101eec
asreekar <49203680+asreekar@users.noreply.github.com>
2021-11-11 16:09:51 -0600

    Update prefix.py

--- prefix.py
+++ prefix.py
@@ -2,11 +2,11 @@ import boto3
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('prefix', help="Prefix")
-parser.add_argument('folder_name', help="folder name")
-parser.add_argument('bucket_name', help="bucket name")
-parser.add_argument('key_id', help="key id")
-parser.add_argument('secret_key', help="secret key")
+parser.add_argument('--prefix', help="Prefix")
+parser.add_argument('--folder_name', help="folder name")
+parser.add_argument('--bucket_name', help="bucket name")
+parser.add_argument('--key_id', help="key id")
+parser.add_argument('--secret_key', help="secret key")
 args = parser.parse_args()
 
 s3 = boto3.client('s3', 
