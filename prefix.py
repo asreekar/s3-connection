f353d06701a3f3d5f8e4fa613f5181c3e2c4b67f
asreekar <49203680+asreekar@users.noreply.github.com>
2021-11-11 13:54:57 -0600

    Update prefix.py

--- prefix.py
+++ prefix.py
@@ -5,13 +5,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('prefix', help="Prefix")
 parser.add_argument('folder_name', help="folder name")
 parser.add_argument('bucket_name', help="bucket name")
-parser.add_argument('key-id', help="key id")
-parser.add_argument('secret-key', help="secret key")
+parser.add_argument('key_id', help="key id")
+parser.add_argument('secret_key', help="secret key")
 args = parser.parse_args()
 
 s3 = boto3.client('s3', 
-                      aws_access_key_id = args.key-id, 
-                      aws_secret_access_key = args.secret-key, 
+                      aws_access_key_id = args.key_id, 
+                      aws_secret_access_key = args.secret_key, 
                       region_name = "us-east-1"
                       )
 
